39984208061e50267f031d6b6d128ceca2d4350c
Joshua Johanan <josh359@gmail.com>
2012-05-10 11:08:49 -0400

    FB is almost setup

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -1,8 +1,12 @@
 <?php
 
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
-{
-
-
+{	
+	 protected function _initConfig()
+	{
+	    $config = new Zend_Config($this->getOptions(), true);
+	    Zend_Registry::set('config', $config);
+	    return $config;
+	}
 }
 
--- library/Facebook/facebook.php
+++ library/Facebook/facebook.php
@@ -21,7 +21,7 @@ require_once "base_facebook.php";
  * Extends the BaseFacebook class with the intent of using
  * PHP sessions to store user ids and access tokens.
  */
-class Facebook extends BaseFacebook
+class Facebook_Facebook extends BaseFacebook
 {
   /**
    * Identical to the parent constructor, except that
